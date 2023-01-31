ab6edc293794fe3698b3988648e87938ae829ade
maruhxn <a23686397@gmail.com>
2023-01-31 20:52:09 +0900

    동네생활

---
+++ pages/api/posts/index.ts
@@ -0,0 +1,76 @@
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  if (req.method === "POST") {
+    const {
+      body: { question, latitude, longitude },
+      session: { user },
+    } = req;
+    const post = await client.post.create({
+      data: {
+        question,
+        latitude,
+        longitude,
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+      },
+    });
+    res.json({
+      ok: true,
+      post,
+    });
+  } else if (req.method === "GET") {
+    const {
+      query: { latitude, longitude },
+    } = req;
+    const latitudeNum = parseFloat(latitude.toString());
+    const longitudeNum = parseFloat(longitude.toString());
+    const posts = await client.post.findMany({
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            avatar: true,
+          },
+        },
+        _count: {
+          select: {
+            wondering: true,
+            answers: true,
+          },
+        },
+      },
+      where: {
+        latitude: {
+          gte: latitudeNum - 0.01,
+          lte: latitudeNum + 0.01,
+        },
+        longitude: {
+          gte: longitudeNum - 0.01,
+          lte: longitudeNum + 0.01,
+        },
+      },
+    });
+    res.json({
+      ok: true,
+      posts,
+    });
+  }
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["GET", "POST"],
+    handler,
+  })
+);
